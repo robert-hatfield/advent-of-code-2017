50b35d39783a830878889a16b425de87f7cf16f3
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-12-07 15:50:01 -0800

    Start work on challenge 3
    I'm having a tough time figuring out how to construct the spiral memory
     object, so I'll leave it as is for now.

---
+++ Advent03.playground/Contents.swift
@@ -0,0 +1,80 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+ --- Day 3: Spiral Memory ---
+ 
+ You come across an experimental new kind of memory stored on an infinite two-dimensional grid.
+ 
+ Each square on the grid is allocated in a spiral pattern starting at a location marked 1 and then counting up while spiraling outward. For example, the first few squares are allocated like this:
+ 
+ 17  16  15  14  13
+ 18   5   4   3  12
+ 19   6   1   2  11
+ 20   7   8   9  10
+ 21  22  23---> ...
+ While this is very space-efficient (no squares are skipped), requested data must be carried back to square 1 (the location of the only access port for this memory system) by programs that can only move up, down, left, or right. They always take the shortest path: the Manhattan Distance between the location of the data and square 1.
+ 
+ For example:
+ 
+ Data from square 1 is carried 0 steps, since it's at the access port.
+ Data from square 12 is carried 3 steps, such as: down, left, left.
+ Data from square 23 is carried only 2 steps: up twice.
+ Data from square 1024 must be carried 31 steps.
+ How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?
+ 
+ Your puzzle input is 277678.
+ 
+
+ */
+
+
+let input = 277678
+
+// Define memory object & nodes as a modified linked list.
+public class MemoryNode<T> {
+    var value: T
+    var neighbor: MemoryNode?
+    
+    public init(value:T) {
+        self.value = value
+    }
+}
+
+public class SpiralMemory<T> {
+    public typealias Node = MemoryNode<T>
+    
+    private var head: Node?
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    public var first: Node? {
+        return head
+    }
+}
+
+let list = SpiralMemory<String>()
+list.isEmpty
+list.first
+
+
+func retrieve(_ input: Int) -> Int? {
+    var result = 0
+    
+    // Figure out length of operation here
+    
+    return result
+}
+
+// Tests
+retrieve(1) == 0
+retrieve(12) == 3
+retrieve(23) == 2
+retrieve(1024) == 31
+
+
+
+
